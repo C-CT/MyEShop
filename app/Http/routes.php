dadf4f70b266b5c89f5ee85c5ac7a520cee767f4
CHEN C.T <cct_address@hotmail.com>
2016-08-03 16:32:41 +0800

    父視圖與子視圖製作

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
@@ -22,3 +22,35 @@ Route::post('/', function () {
 Route::delete('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get("/","myResourceController@index");
+
+//Route::get("/show/{name}","myResourceController@show");
+
+//    Route::get('/', "myController@index");
+
+    Route::get('/contact_us', "myController@contact_us");
+
+    Route::get('/login', "myController@login");
+
+    Route::get('/logout', "myController@logou");
+
+    Route::get('/products', "myController@products");
+
+    Route::get('/products/category', "myController@products_category");
+
+    Route::get('/products/brands', "myController@products_brands");
+
+    Route::get('/products/details/{id}', "myController@products_details");
+
+    Route::get('/blog', "myController@blog");
+
+    Route::get('/blog/post/{id}', "myController@blog_post");
+
+    Route::get('/search/{key_word}',"myController@search");
+
+    Route::get('/cart', "myController@cart");
+
+    Route::get('/checkout', "myController@checkout");
+    
