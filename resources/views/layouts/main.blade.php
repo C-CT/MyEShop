93b8d6af7e9dcc7248e43003e1b81485d08c321a
CHEN C.T <cct_address@hotmail.com>
2016-08-04 14:02:46 +0800

    1. main.blade.php 中切出一部分程式至patrs->sidebar中. 2. main.blade.php 中關閉一些重複功能的選項.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -88,6 +88,7 @@
 <!--							<a href="index.html"><img src="images/home/logo.png" alt="" /></a>-->
 							<a href="{{url("/")}}"><img src="images/home/logo.png" alt="" /></a>
 						</div>
+<!--
 						<div class="btn-group pull-right">
 							<div class="btn-group">
 								<button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
@@ -111,12 +112,13 @@
 								</ul>
 							</div>
 						</div>
+-->
 					</div>
 					<div class="col-sm-8">
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
 								<li><a href="#"><i class="fa fa-user"></i> Account</a></li>
-								<li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
+<!--								<li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>-->
 								<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
 								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
 								<li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
@@ -141,24 +143,32 @@
 						</div>
 						<div class="mainmenu pull-left">
 							<ul class="nav navbar-nav collapse navbar-collapse">
+							
+<!--							/*Home*/-->
 <!--								<li><a href="index.html" class="active">Home</a></li>-->
 								<li><a href="{{url("/")}}" class="active">Home</a></li>
+<!--							/*Shop*/-->
 								<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
                                         <li><a href="shop.html">Products</a></li>
 										<li><a href="product-details.html">Product Details</a></li> 
+<!--
 										<li><a href="checkout.html">Checkout</a></li> 
 										<li><a href="cart.html">Cart</a></li> 
 										<li><a href="login.html">Login</a></li> 
+-->
                                     </ul>
                                 </li> 
+<!--                             /*Blog*/-->
 								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
                                         <li><a href="blog.html">Blog List</a></li>
-										<li><a href="blog-single.html">Blog Single</a></li>
+<!--										<li><a href="blog-single.html">Blog Single</a></li>-->
                                     </ul>
                                 </li> 
-								<li><a href="404.html">404</a></li>
+<!--                             /*404*/-->
+<!--								<li><a href="404.html">404</a></li>-->
+<!--                             /*Contact*/-->
 								<li><a href="contact-us.html">Contact</a></li>
 							</ul>
 						</div>
